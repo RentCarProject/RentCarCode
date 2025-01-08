85a995b68192ddff3fec481c9a29e17388c1bcdf
kimkim1497 <hunoohun92@gmail.com>
2025-01-08 09:45:16 +0900

    Delete BN directory

--- BN/src/main/java/com/project/rentcar/domain/repository/PaymentsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.rentcar.domain.repository;
-
-import com.project.rentcar.domain.entity.Payments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentsRepository extends JpaRepository<Payments, String> {
-}
